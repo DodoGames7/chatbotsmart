93f2f56e921d9008c711c20752af1e6d279c5328
sapcraft-org <76895953+sapcraft-org@users.noreply.github.com>
2021-06-22 18:40:01 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const { Client } = require("discord.js");
+const client = new Client({ intents: 513 });
+const smartestchatbot = require("smartestchatbot");
+var x = new smartestchatbot.Client();
+client.on("ready", () => {
+  console.log("Ready for chatting!| Bot by dodoGames");
+});
+client.on("message", async message => {
+  // when client detects a message
+  if (message.author.bot) return; // if the author of the message is a bot ignore the case
+  message.content = message.content // let content ( a variable used to fetch response ) be equal to the message's content
+    .replace(/@(everyone)/gi, "everyone")
+    .replace(/@(here)/gi, "here");
+  if (message.content.includes(`@`)) {
+    return message.reply(
+      `**:x: imagine pinging me -_-**`
+    );
+  }
+  message.channel.startTyping();
+  if (!message.content)
+    return message.channel.send("I can only reply to text messages");
+  x.chat({
+    message: message.content,
+    name: client.user.username,
+    owner: "dodoGames", // Add Owner Name Here
+    user: message.author.id,
+    language: "en" // You can change the language here ( auto ) states it will detect your language and prepare a response in english for you
+  }).then(reply => {
+    message.reply({
+      embed: {
+        title: "ChatBot",
+        color: "#5539cc",
+        description: `**${reply}**`,
+        timestamp: new Date(),
+        footer: {
+          text: `${client.user.username}`
+        }
+      }
+    });
+  });
+  message.channel.stopTyping();
+});
+client.login('Nzk1MTE4NTQ2ODQ1ODI3MDgz.X_Et2g.qKmuVp2K-X5phggmScBWEfgWkeY'); //login using the token
